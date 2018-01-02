dfec01b63eb73f1c55db5bdb3121bf71984ca39e
永山 大志 <tnagayama@geechs.com>
2018-01-02 19:47:25 +0900

    first

---
+++ Assets/Racket.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour {
+
+	[SerializeField] private Rigidbody rigitBody;
+
+	private float Accel = 1.0f;
+
+	// Update is called once per frame
+	void Update () {
+		float a = Input.GetAxisRaw( "Horizontal" );
+		Debug.Log(a);
+		rigitBody.AddForce(
+			transform.right * Input.GetAxisRaw( "Horizontal" ) * Accel,
+			ForceMode.Impulse);
+		
+	}
+}
---
+++ Assets/firstSpeed.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstSpeed : MonoBehaviour {
+
+	[SerializeField] private Rigidbody rigitbody;
+
+	// Use this for initialization
+	void Start () {
+		this.rigitbody.AddForce(new Vector3(0,0,1000));
+	}
+		
+
+	
+
+}
