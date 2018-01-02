32d8cdcbac7840c56dc68a03544b82bfad2bbb07
busido90 <nagatai0120@gmail.com>
2018-01-02 23:55:07 +0900

    aaa

---
+++ Assets/obstacle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacle : MonoBehaviour {
+
+[SerializeField] private OnCollisionEnter OnCollisionEnter;
+
+	// Use this for initialization
+	void Start () {
+		this.OnCollisionEnter(Collision: collision);
+             destroy(gameobject);
+        		
+	}
+	
+	
+  
